9418ccb3a6fd8e4b67d1b1655bee2cbedead836f
William Gottschalk <will.gottschalk@gmail.com>
2015-11-10 10:14:28 -0800

    fixed backend middleware. need to update aggregation function

--- server/foursquareController.js
+++ server/foursquareController.js
@@ -1,45 +1,34 @@
 var request = require('request');
-var mydb = require('./postgres.js');
+var trueReview = require('./postgres.js');
 var Promise = require('bluebird');
+
+var queryURL = 'https://api.foursquare.com/v2/venues/explore?ll=33.97914,-118.41480705731&radius=8000&query=food&client_id=2QFBHZM23MLCX0WRQATSMNV1XSV41LONUF1AOUEHADZVZZBI&client_secret=5TXGRZBINMI4KWOPFFELJ1UUUP3YMRM0VKZJ4XKMRTMQZ4XU&v=20151105';
+
 var fourSquareController = {
-
 	getData: function(req, res, next) {
-		var queryURL = 'https://api.foursquare.com/v2/venues/explore?ll=33.97914,-118.41480705731&radius=8000&query=food&client_id=2QFBHZM23MLCX0WRQATSMNV1XSV41LONUF1AOUEHADZVZZBI&client_secret=5TXGRZBINMI4KWOPFFELJ1UUUP3YMRM0VKZJ4XKMRTMQZ4XU&v=20151105';
-
-// console.log("HERE);")
-		var foursquareData = [];
-
 		request(queryURL, function(error, response, data) {
 			if (error) {
 				console.log(error);
 			}
-			var promises = new Promise(function(resolve, reject) {
-				var stats = JSON.parse(data).response.groups[0].items;
-
-				for(var i = 0; i < stats.length; i++) {
-					var obj = {};
-					obj.name = stats[i].venue.name;
-			        obj.rating = stats[i].venue.rating / 2;
-			        obj.review_count = stats[i].venue.ratingSignals;
-			        obj.lat = stats[i].venue.location.lat;
-			        obj.long = stats[i].venue.location.lng;
-			        obj.address = stats[i].venue.location.address;
-			        obj.city = stats[i].venue.location.city;
-			        obj.state = stats[i].venue.location.state;
-			        obj.postal_code = stats[i].venue.location.postalCode;
-					foursquareData.push(obj);
-				}
-				resolve(foursquareData);
+			var stats = JSON.parse(data).response.groups[0].items;
+			var restaurants = stats.map(function(stat) {
+				return {
+					name: stat.venue.name,
+					rating: stat.venue.rating / 2,
+					review_count: stat.venue.ratingSignals,
+					lat: stat.venue.location.lat,
+					lon: stat.venue.location.lon,
+					address: stat.venue.location.address[0],
+					city: stat.venue.location.city,
+					state: stat.venue.location.state,
+					postal_code: stat.venue.location.postalCode
+			 	};
 			});
-			promises.then(function(result) {
-				mydb.foursquare(result);
-				// console.log(result);
-				setTimeout(function(){next();}, 4000);
-			});
+			trueReview.fourSqDatabaseSync(restaurants, 'FoursquareModel');
 		});
-	}
-
+	}	
 };
+
 
     // get request in the url returns the huge data on the site
     // learn the parameters of the foursquare
